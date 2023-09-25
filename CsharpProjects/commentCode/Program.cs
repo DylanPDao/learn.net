9cc965e7f14b32b90d998a7b26266827b6d665ee
Dylan Dao <dylanpdao@gmail.com>
2023-09-25 15:18:04 -0700

    finished challenge

---
+++ CsharpProjects/commentCode/Program.cs
@@ -0,0 +1,15 @@
+﻿// reverse a string and count the number of times a particular character appears
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int letterCount = 0;
+
+foreach (char letter in charMessage) { if (letter == 'o') { letterCount++; } }
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");

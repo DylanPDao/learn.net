8b2fbbe4d8537b2f9acc2ebeb0992b09dd0768af
Dylan Dao <dylanpdao@gmail.com>
2023-10-13 08:26:28 -0700

    refractored code for readibility

--- CsharpProjects/evaluateBoolean/Program.cs
+++ CsharpProjects/evaluateBoolean/Program.cs
@@ -1,23 +1,53 @@
-﻿string permission = "Manager";
-int level = 4;
+﻿// string permission = "Manager";
+// int level = 4;
 
-if (permission.Contains("Admin") && level > 55)
+// if (permission.Contains("Admin") && level > 55)
+// {
+//   Console.WriteLine("Welcome, Super Admin User");
+// }
+// else if (permission.Contains("Admin") && level <= 55)
+// {
+//  Console.WriteLine("Welcome, Admin User");
+// }
+// else if (permission.Contains("Manager") && level >= 20)
+// {
+//  Console.WriteLine("Contact Admin for access");
+// }
+// else if (permission.Contains("Manager") && level < 20)
+// {
+//  Console.WriteLine("You do not have sufficient privileges");
+// }
+// else
+// {
+//    Console.WriteLine("You do not have sufficient privileges.");
+// }
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
 {
-  Console.WriteLine("Welcome, Super Admin User");
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
 }
-else if (permission.Contains("Admin") && level <= 55)
+else if (permission.Contains("Manager"))
 {
- Console.WriteLine("Welcome, Admin User");
-}
-else if (permission.Contains("Manager") && level >= 20)
-{
- Console.WriteLine("Contact Admin for access");
-}
-else if (permission.Contains("Manager") && level < 20)
-{
- Console.WriteLine("You do not have sufficient privileges");
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
 }
 else
 {
-   Console.WriteLine("You do not have sufficient privileges.");
+    Console.WriteLine("You do not have sufficient privileges.");
 }

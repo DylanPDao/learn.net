5d1ffb7a49779e146bd40cc0f9f6c050304ac73a
Dylan Dao <dylanpdao@gmail.com>
2023-10-13 08:23:41 -0700

    added admin if else statements

--- CsharpProjects/evaluateBoolean/Program.cs
+++ CsharpProjects/evaluateBoolean/Program.cs
@@ -1,2 +1,15 @@
 ﻿string permission = "Admin|Manager";
 int level = 55;
+
+if (permission.Contains("Admin") && level > 55)
+{
+  Console.WriteLine("Welcome, Super Admin User");
+}
+else if (permission.Contains("Admin") && level <= 55)
+{
+ Console.WriteLine("Welcome, Admin User");
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+ Console.WriteLine("Contact Admin for access");
+}

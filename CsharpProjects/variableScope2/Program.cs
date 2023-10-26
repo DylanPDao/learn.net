9dccefa7438a40d79a921239a56835c1e48d93a7
Dylan Dao <dylanpdao@gmail.com>
2023-10-26 14:11:16 -0700

    initialized variables out of the foreach loop

--- CsharpProjects/variableScope2/Program.cs
+++ CsharpProjects/variableScope2/Program.cs
@@ -1,17 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
 foreach (int number in numbers)
 {
-    int total;
 
     total += number;
 
     if (number == 42)
     {
        bool found = true;
-
     }
 
 }

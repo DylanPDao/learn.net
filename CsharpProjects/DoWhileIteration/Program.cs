10544a642b3d7f4c55f2bfe503c2bb368d55431e
Dylan Dao <dylanpdao@gmail.com>
2023-12-19 16:03:14 -0800

    finished exercise

---
+++ CsharpProjects/DoWhileIteration/Program.cs
@@ -0,0 +1,17 @@
+﻿int heroHealth = 10;
+int monsterHealth = 15;
+Random random = new Random();
+int attack = random.Next(1,11);
+
+do{
+  attack = random.Next(1,11);
+  monsterHealth -= attack;
+  Console.WriteLine($"DMG: {attack} || Monster HP: {monsterHealth}");
+  if (monsterHealth > 0) {
+    attack = random.Next(1,11);
+    heroHealth -= attack;
+    Console.WriteLine($"DMG: {attack} || Hero HP: {heroHealth}");
+  }
+} while (heroHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine(heroHealth > monsterHealth ? "Hero Wins!" : "Monster Wins!");
